53b53760ed0db048c365abc9f31f37918c9bc17b
shri008 <shrikrushna369@gmail.com>
2022-08-29 15:40:58 +0530

    added new build directory

---
+++ make_test/build/CMakeFiles/calculator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/shrikrushna/shri_git/shri_public/make_test/add.c" "/home/shrikrushna/shri_git/shri_public/make_test/build/CMakeFiles/calculator.dir/add.c.o"
+  "/home/shrikrushna/shri_git/shri_public/make_test/div.c" "/home/shrikrushna/shri_git/shri_public/make_test/build/CMakeFiles/calculator.dir/div.c.o"
+  "/home/shrikrushna/shri_git/shri_public/make_test/main.c" "/home/shrikrushna/shri_git/shri_public/make_test/build/CMakeFiles/calculator.dir/main.c.o"
+  "/home/shrikrushna/shri_git/shri_public/make_test/mul.c" "/home/shrikrushna/shri_git/shri_public/make_test/build/CMakeFiles/calculator.dir/mul.c.o"
+  "/home/shrikrushna/shri_git/shri_public/make_test/sub.c" "/home/shrikrushna/shri_git/shri_public/make_test/build/CMakeFiles/calculator.dir/sub.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

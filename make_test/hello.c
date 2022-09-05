612280e6b74c0abe170573fb247d10556fefa167
shri008 <shrikrushna369@gmail.com>
2022-09-05 12:54:44 +0530

    modified

--- make_test/hello.c
+++ make_test/hello.c
@@ -2,18 +2,11 @@
 
 void main()
 {
-<<<<<<< HEAD
 printf("sema charging");
-=======
 printf("blink charging");
->>>>>>> master
     printf("this is modification");
     printf("hello");
     printf("hello");
     printf("hello");
-    printf("hello");
-<<<<<<< HEAD
     printf("hello semaconnect");
-=======
->>>>>>> master
 }

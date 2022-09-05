623e684b29e0a776447a1bd48b5fcd4364391fd0
shri008 <shrikrushna369@gmail.com>
2022-09-05 14:53:24 +0530

    hgaiushcd

--- make_test/hello.c
+++ make_test/hello.c
@@ -5,6 +5,8 @@ void main()
 printf("sema charging");
 printf("blink charging");
     printf("this is modification");
+    printf("this is modification");
+    printf("this is modification");
     printf("hello");
     printf("hello semaconnect");
 }

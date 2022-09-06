6058bd9ac5a4182f8788aeb3d274957e62c7689c
shri008 <shrikrushna369@gmail.com>
2022-09-06 11:18:32 +0530

    modified

--- make_test/hello.c
+++ make_test/hello.c
@@ -6,8 +6,6 @@ printf("sema charging");
 printf("blink charging");
     printf("this is modification");
     printf("this is modification");
-<<<<<<< HEAD
-=======
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");
@@ -16,9 +14,6 @@ printf("blink charging");
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");
-    printf("this is modification");
-    printf("this is modification");
->>>>>>> 789283a7759b4326d27aba88965bfd6079857c05
     printf("hello");
     printf("hello semaconnect");
 }

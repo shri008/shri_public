41cdecfeebdd6ab82adfa27378cf1932d10ee64c
shri008 <shrikrushna369@gmail.com>
2022-09-06 11:24:40 +0530

    modified

--- make_test/hello.c
+++ make_test/hello.c
@@ -11,7 +11,6 @@ printf("blink charging");
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");
-    printf("this is modification");
     printf("hello");
     printf("hello semaconnect");
 }

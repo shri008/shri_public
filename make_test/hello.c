2bf69b07673f2e1732e4c70cac31f2b9c5b562b9
shrikrushna <shrikrushna.chavan@semaconnect.com>
2022-09-09 13:05:14 +0530

    modified hello.c

--- make_test/hello.c
+++ make_test/hello.c
@@ -10,6 +10,7 @@ printf("blink charging");
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");
+    printf("this is modification");
     printf("hello");
     printf("hello semaconnect");
 }

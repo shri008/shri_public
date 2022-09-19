aad0727b816d2a76946ec62baef9ac3331436ce6
shrikrushna <shrikrushna.chavan@semaconnect.com>
2022-09-19 11:38:32 +0530

    this is thirdtag

--- make_test/hello.c
+++ make_test/hello.c
@@ -5,7 +5,7 @@ void main()
 printf("sema charging");
 printf("blink charging");
     printf("this is new tag 19sep_02");
-    printf("this is modification");
+    printf("this is third tag 19sep_03");
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");

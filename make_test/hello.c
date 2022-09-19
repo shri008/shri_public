cfb24591e5f856a4e1ad44ebbbc66d3c0f936f8f
shrikrushna <shrikrushna.chavan@semaconnect.com>
2022-09-19 11:14:52 +0530

    tag added

--- make_test/hello.c
+++ make_test/hello.c
@@ -4,6 +4,7 @@ void main()
 {
 printf("sema charging");
 printf("blink charging");
+    printf("this is new tag 19sep_02");
     printf("this is modification");
     printf("this is modification");
     printf("this is modification");
